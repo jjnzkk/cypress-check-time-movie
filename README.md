2a8e2585955526f8c2ae1153043e478a3a182659
Pichavarit <pichavarit.p@nellikaconsulting.com>
2018-11-07 10:42:06 +0700

    init commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# cypress-check-time-movie
+
+<p align="center">
+  <img src="https://cdn-images-1.medium.com/max/1600/1*zj4Bx6WYiI4XKC6zt7QvhA.png"/>
+</p>
+
+### Getting Started
+
+```
+git clone https://github.com/jjnzkk/cypress-check-time-movie
+```
+
+### Install npm package
+
+```
+npm install
+```
+
+### Enjoy!
+```
+npm run cypress:open
+```
+
+[Documentation](https://medium.com/@pichavarit.p/%E0%B9%80%E0%B8%A3%E0%B8%B4%E0%B9%88%E0%B8%A1%E0%B8%95%E0%B9%89%E0%B8%99%E0%B9%80%E0%B8%82%E0%B8%B5%E0%B8%A2%E0%B8%99-automated-test-%E0%B8%94%E0%B9%89%E0%B8%A7%E0%B8%A2-cypress-io-%E0%B9%81%E0%B8%9A%E0%B8%9A%E0%B8%A5%E0%B8%87%E0%B8%A1%E0%B8%B7%E0%B8%AD%E0%B8%97%E0%B8%B3-dcda05f3a585)
---
+++ cypress/integration/movie/check_time_movie.spec.ts
@@ -0,0 +1,61 @@
+const url = 'https://www.sfcinemacity.com'
+const todayDate = Cypress.moment().format('DD MMM YYYY')
+const nowTime = Cypress.moment().format('HH:mm')
+const expectTime = Cypress.moment()
+  .add(1, 'hours')
+  .format('HH:mm')
+const nameMovie = 'The Nutcracker and the Four Realms'
+const locationMovie = 'SFX CINEMA Central Rama 9'
+
+describe('Check Time Movie', () => {
+  it('Go to url', () => {
+    cy.visit(url)
+  })
+  it('Change language', () => {
+    cy.get('[class="lang-switcher"]>li').each($el => {
+      if ($el.get(0).innerText === 'ENG') {
+        cy.wrap($el).click()
+      }
+    })
+    cy.get('[class="top-navigation"]').contains('Login/Sign up')
+  })
+  it('Select Cinema', () => {
+    cy.get('[class="button dropdown-button"]')
+      .contains('Select Cinema')
+      .click()
+    cy.contains(locationMovie).click()
+  })
+  it('Select Movie', () => {
+    cy.get('[class="button dropdown-button"]')
+      .contains('All Movie')
+      .click()
+    cy.get('h3[class="name"]')
+      .contains(nameMovie)
+      .click()
+    cy.get('[class="button showtime-button"]')
+      .contains('Showtime')
+      .click()
+  })
+  it('Check Date Movie', () => {
+    cy.get('[class="selected"]>p').contains(todayDate)
+  })
+  it('Check Time Movie', () => {
+    cy.get('[class="showtime-list"]>div')
+      .children()
+      .children()
+      .children()
+      .children()
+    cy.get('[class="time-list"]>li').each($list => {
+      if (
+        $list.get(0).innerText >=  nowTime &&
+        $list.get(0).innerText <= expectTime
+      ) {
+        cy.wrap($list.children()).click()
+        return
+      }
+    })
+  })
+  it('Check Page Change', () => {
+     cy.contains('Selected Seat')
+  })
+})
